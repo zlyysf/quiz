1200947f4cd4c98dfb12ef22b0d1a416eac1f042
mliu <mliu@tagged.com>
2013-01-30 13:02:39 +0800

    Create Quiz Project for iPhone.

---
+++ iphone/trunk/QuizGame/QuizGame/LZViewController.m
@@ -0,0 +1,29 @@
+//
+//  LZViewController.m
+//  QuizGame
+//
+//  Created by liu miao on 1/30/13.
+//  Copyright (c) 2013 lingzhi mobile. All rights reserved.
+//
+
+#import "LZViewController.h"
+
+@interface LZViewController ()
+
+@end
+
+@implementation LZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iphone/trunk/QuizGame/QuizGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuizGame
+//
+//  Created by liu miao on 1/30/13.
+//  Copyright (c) 2013 lingzhi mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LZAppDelegate class]));
+    }
+}
