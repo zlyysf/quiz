42f05506640472fff8b875cfd46974bc12a6821d
mliu <mliu@tagged.com>
2013-01-31 10:49:26 +0800

    adjust project composition。

---
+++ iphone/trunk/QuizGame/GADMasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  GADMasterViewController.h
+//  QuizGame
+//
+//  Created by liu miao on 1/30/13.
+//  Copyright (c) 2013 lingzhi mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GADBannerView.h"
+@interface GADMasterViewController : UIViewController {
+    GADBannerView *adBanner_;
+    BOOL didCloseWebsiteView_;
+    BOOL isLoaded_;
+    id currentDelegate_;
+}
++(GADMasterViewController *)singleton;
+-(void)resetAdView:(UIViewController<GADBannerViewDelegate> *)rootViewController;
+@end
---
+++ iphone/trunk/QuizGame/LZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LZAppDelegate.h
+//  QuizGame
+//
+//  Created by liu miao on 1/30/13.
+//  Copyright (c) 2013 lingzhi mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
