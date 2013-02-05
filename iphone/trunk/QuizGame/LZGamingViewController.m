642c455d7a46e20004381c65bb48d99c83b4b1b6
mliu <mliu@tagged.com>
2013-02-05 14:49:42 +0800

    update gaming performance.

--- iphone/trunk/QuizGame/LZGamingViewController.m
+++ iphone/trunk/QuizGame/LZGamingViewController.m
@@ -10,7 +10,9 @@
 #import <QuartzCore/QuartzCore.h>
 #import "LZDataAccess.h"
 #define kTotalOptionCount 3
-@interface LZGamingViewController ()<LZPlayViewDelegate>
+#define kSelectedAnswerInterval 0.5
+#define kSwitchNextQuestionInterval 0.5
+@interface LZGamingViewController ()<LZPlayViewDelegate,UIAlertViewDelegate>
 @property(nonatomic,strong)NSArray *quizArray;
 @property(nonatomic,assign)int currentQuizIndex;
 @property(nonatomic,assign)int totalQuizCount;
@@ -53,34 +55,26 @@
     currentQuizIndex = 0;
     answeredWrongCount = 0;
     answeredRightCount = 0;
+    
     playView1 = [[LZPlayView alloc]initWithFrame:playFrame];
     playView1.delegate = self;
     
     playView2 = [[LZPlayView alloc]initWithFrame:playFrame];
     playView2.delegate = self;
-    self.currentOptionArray = [[NSMutableArray alloc]initWithCapacity:1+kTotalOptionCount];
-//    NSArray *option1 = [[NSArray alloc]initWithObjects:@"icon_2.jpg",@"icon_3.jpg",@"icon_4.jpg", nil];
-//    NSDictionary *quiz1 = [[NSDictionary alloc]initWithObjectsAndKeys:@"松子",@"questionWord", @"icon_1.jpg",@"answerPic",option1,@"options", nil];
-//    NSArray *option2 = [[NSArray alloc]initWithObjects:@"icon_1.jpg",@"icon_3.jpg",@"icon_4.jpg", nil];
-//    NSDictionary *quiz2 = [[NSDictionary alloc]initWithObjectsAndKeys:@"阿迪达斯",@"questionWord",@"icon_2.jpg",@"answerPic",option2,@"options", nil];
-//    NSArray *option3 = [[NSArray alloc]initWithObjects:@"icon_2.jpg",@"icon_1.jpg",@"icon_4.jpg", nil];
-//    NSDictionary *quiz3 = [[NSDictionary alloc]initWithObjectsAndKeys:@"老鹰",@"questionWord", @"icon_3.jpg",@"answerPic",option3,@"options",nil];
-//    NSArray *option4 = [[NSArray alloc]initWithObjects:@"icon_2.jpg",@"icon_3.jpg",@"icon_1.jpg", nil];
-//    NSDictionary *quiz4 = [[NSDictionary alloc]initWithObjectsAndKeys:@"柠檬",@"questionWord", @"icon_4.jpg",@"answerPic",option4,@"options",nil];
     
+    self.currentOptionArray = [[NSMutableArray alloc]initWithCapacity:1+kTotalOptionCount];
     self.quizArray = [[LZDataAccess singleton] getGroupQuizzes:currentGroupKey];
     NSLog(@"quizArray %@",[self.quizArray description]);
-    //-(FMResultSet *) getGroupQuiz:(NSString *)grpkey;
+
     totalQuizCount = [self.quizArray count];
     [self initializeOrderArray:totalQuizCount];
     [self displayGameView:currentQuizIndex];
     self.rightIconImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, kAnswerButtonSideLength, kAnswerButtonSideLength)];
-    [self.rightIconImageView setImage:[UIImage imageNamed:@"correct.png"]];
+    [self.rightIconImageView setImage:[UIImage imageNamed:@"selected_right.png"]];
+    self.rightIconImageView.contentMode = UIViewContentModeScaleToFill;
     self.wrongIconImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, kAnswerButtonSideLength, kAnswerButtonSideLength)];
-    [self.wrongIconImageView setImage:[UIImage imageNamed:@"wrong.png"]];
-    
-    
-   
+    [self.wrongIconImageView setImage:[UIImage imageNamed:@"selected_wrong.png"]];
+    self.wrongIconImageView.contentMode = UIViewContentModeScaleToFill;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -148,7 +142,6 @@
         {
             NSString *pic = [[option objectAtIndex:i] objectForKey:@"answerPic"];
             [self.currentOptionArray addObject:pic];
-            //[self.currentOptionArray addObject:pic];
         }
         NSLog(@"before random currentOptionArray %@",[self.currentOptionArray description]);
         for(int i=1;i<[self.currentOptionArray count];i++)
@@ -167,7 +160,6 @@
         }
         if (index%2 == 0)//set play1
         {
-            //[self setPlayViewContent:playView1];
             self.playView1.progressLabel.text = [NSString stringWithFormat:@"%d / %d",currentQuizIndex+1,totalQuizCount];
             self.playView1.questionLabel.text = [quiz objectForKey:@"questionWord"];
             UIButton *helpButton = (UIButton*)[self.playView1 viewWithTag:kCutWrongButtonTag];
@@ -202,8 +194,10 @@
         
         [UIView beginAnimations:nil context:nil];
         [UIView setAnimationCurve:UIViewAnimationCurveLinear];
-        [UIView setAnimationDuration:0.5];
+        [UIView setAnimationDelegate:self];
+        [UIView setAnimationDuration:kSwitchNextQuestionInterval];
         [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view cache:YES];
+        [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
         if (index%2 == 0) {
             [playView2 removeFromSuperview];
             [self.view addSubview:playView1];
@@ -217,7 +211,11 @@
 
     }
 }
-
+- (void)animationDidStop:(NSString*)animationID finished:(NSNumber*)finished context:(void*)context
+{
+    [self.playView1 setUserInteractionEnabled:YES];
+    [self.playView2 setUserInteractionEnabled:YES];
+}
 #pragma -mark LZPlay View Delegate
 - (void)playViewButtonClicked:(UIButton*)button
 {
@@ -233,29 +231,42 @@
     {
         if(tag == kWinButtonTag)
         {
-            NSString *message = [NSString stringWithFormat:@"正确 tapped button tag %d",button.tag];
-            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:nil message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-            [alert show];
+            NSNumber *current= [self.orderArray objectAtIndex:currentQuizIndex];
+            NSDictionary *quiz = [self.quizArray objectAtIndex:[current integerValue]];
+            NSString *answerPic = [quiz objectForKey:@"answerPic"];
+            for(int i = 0 ;i<[self.currentOptionArray count];i++)
+            {
+                NSString *aOption = [self.currentOptionArray objectAtIndex:i];
+                if ([aOption isEqualToString:answerPic])
+                {
+                    if (currentQuizIndex%2 == 0)//set play1
+                    {
+                        UIButton *button = (UIButton*)[self.playView1 viewWithTag:200+i];
+                        [self.playView1 addSubview:self.rightIconImageView];
+                        rightIconImageView.center = button.center;
+                    }
+                    else
+                    {
+                        UIButton *button = (UIButton *)[self.playView2 viewWithTag:200+i];
+                        [self.playView2 addSubview:self.rightIconImageView];
+                        rightIconImageView.center = button.center;
+                        
+                    }
+                break;
+
+                }
+            }
             answeredRightCount++;
             self.topNavView.correctCountLabel.text = [NSString stringWithFormat:@"%d",answeredRightCount];
-            /*reach the answer the last question*/
-            if (currentQuizIndex == totalQuizCount-1)
-            {
-                //display end view then get back to the group list view
-                //lock or not lock the next level or next package
-            }
+            [self.playView1 setUserInteractionEnabled:NO];
+            [self.playView2 setUserInteractionEnabled:NO];
+            [self performSelector:@selector(displayNext) withObject:nil afterDelay:kSelectedAnswerInterval];
 
-            else if(currentQuizIndex < totalQuizCount-1)
-            {
-                currentQuizIndex++;
-                [self displayGameView:currentQuizIndex];
-            }
         }
         if(tag == kCutWrongButtonTag)
         {
             
             int remainOption = rand()%kTotalOptionCount;
-            //NSNumber *current= [self.orderArray objectAtIndex:index];
             NSNumber *current= [self.orderArray objectAtIndex:currentQuizIndex];
             NSDictionary *quiz = [self.quizArray objectAtIndex:[current integerValue]];
             if (currentQuizIndex%2 == 0)
@@ -270,7 +281,7 @@
             }
             NSString *right = [quiz objectForKey:@"answerPic"];
             NSArray *option = [quiz objectForKey:@"options"];
-            NSString *remainOptionKey = [option objectAtIndex:remainOption];
+            NSString *remainOptionKey = [[option objectAtIndex:remainOption] objectForKey:@"answerPic"];
             for(int i = 0 ;i<[self.currentOptionArray count];i++)
             {
                 NSString *aOption = [self.currentOptionArray objectAtIndex:i];
@@ -279,7 +290,6 @@
                     if (currentQuizIndex%2 == 0)//set play1
                     {
                         UIButton *button = (UIButton*)[self.playView1 viewWithTag:200+i];
-                        //[self.playView1 viewWithTag:kCutWrongButtonTag]
                         button.hidden = YES;
                     }
                     else
@@ -290,7 +300,6 @@
                     
                 }
             }
-            //[self.currentOptionArray removeAllObjects];
 
         }
         if(tag == kAskFriendsButtonTag)
@@ -311,11 +320,9 @@
         NSString *selectedPic = [self.currentOptionArray objectAtIndex:selected];
         NSNumber *current= [self.orderArray objectAtIndex:currentQuizIndex];
         NSDictionary *quiz = [self.quizArray objectAtIndex:[current integerValue]];
-        //NSLog(@"quiz content %@",quiz);
         NSString *answerPic = [quiz objectForKey:@"answerPic"];
         if([selectedPic isEqualToString:answerPic])
         {
-            NSString *message = [NSString stringWithFormat:@"正确 tapped button tag %d",button.tag];
             if (currentQuizIndex%2 == 0)//set play1
             {
                 UIButton *button = (UIButton*)[self.playView1 viewWithTag:tag];
@@ -330,15 +337,11 @@
                 rightIconImageView.center = button.center;
                 
             }
-
-            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:nil message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-            [alert show];
             answeredRightCount++;
             self.topNavView.correctCountLabel.text = [NSString stringWithFormat:@"%d",answeredRightCount];
         }
         else
         {
-            NSString *message = [NSString stringWithFormat:@"错误 tapped button tag %d",button.tag];
             if (currentQuizIndex%2 == 0)//set play1
             {
                 UIButton *button = (UIButton*)[self.playView1 viewWithTag:tag];
@@ -353,21 +356,24 @@
                 wrongIconImageView.center = button.center;
                 
             }
-            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:nil message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-            [alert show];
             answeredWrongCount++;
             self.topNavView.wrongCountLabel.text = [NSString stringWithFormat:@"%d",answeredWrongCount];
         }
-        /*reach the answer the last question*/
-        [self performSelector:@selector(displayNext) withObject:nil afterDelay:0.5];
+        [self.playView1 setUserInteractionEnabled:NO];
+        [self.playView2 setUserInteractionEnabled:NO];
+        [self performSelector:@selector(displayNext) withObject:nil afterDelay:kSelectedAnswerInterval];
     }
 }
 - (void)displayNext
 {
+
     if (currentQuizIndex == totalQuizCount-1)
     {
         //display end view then get back to the group list view
         //lock or not lock the next level or next package
+        NSString *message = [NSString stringWithFormat:@"total quiz : %d  right : %d  wrong : %d",totalQuizCount,answeredRightCount,answeredWrongCount];
+        UIAlertView *endAlert = [[UIAlertView alloc]initWithTitle:@"Quiz Finished" message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [endAlert show];
     }
     else if(currentQuizIndex < totalQuizCount-1)
     {
@@ -376,5 +382,12 @@
     }
 
 }
+-(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == alertView.cancelButtonIndex)
+    {
+        [self backButtonTapped];
+    }
 
+}
 @end
