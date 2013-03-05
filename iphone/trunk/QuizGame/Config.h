c805dccc38c8656f0e278568aebe03531e4ba4b1
mliu <mliu@tagged.com>
2013-03-05 17:10:25 +0800

    add description for the key in userdefaults in config.h

--- iphone/trunk/QuizGame/Config.h
+++ iphone/trunk/QuizGame/Config.h
@@ -12,3 +12,31 @@
 #define IsEnvironmentProd NO //isEnvProd = YES to use prod,NO to use stage.
 
 #endif
+//description for key in Userdefauls
+/*
+key: appKey = appName + appVersion 
+value: NO means app has not initialized for the first time ; YES means app data has already initialized.
+*/
+
+/*
+key: @"LZSoundOn" 
+value: YES means app sound on ; NO means app sound off.
+*/
+
+/*
+key: @"LZAdsOff"  
+value: YES means not show ads; NO means show ads.
+*/
+
+/*
+key: productIdentifier (for the non-cunsumble product in store)
+ @"com.lingzhi.QuizAwsome.removeads",
+ @"com.lingzhi.QuizAwsome.unlockallpackages",
+ @"com.lingzhi.QuizAwsome.unlockfortune",
+ @"com.lingzhi.QuizAwsome.unlockhealth",
+ @"com.lingzhi.QuizAwsome.unlockelectronic",
+ @"com.lingzhi.QuizAwsome.unlocksportsclub",
+ @"com.lingzhi.QuizAwsome.unlockfooddrink",
+ 
+value: YES means user has already purchased this product; NO means user has not purchased this product or has not restore this product.
+*/
